ef8eb996f1c7c13997a8c9586c521156b94e233f
Chris <cmecinski@gmail.com>
2022-07-25 17:07:42 -0500

    condensed code todo: optimization

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ class Emoji {
 		this.speedX = 0;
 		this.speedY = 0;
 		this.markedForDeletion = false;
+		this.targetLocked = false;
 	}
 	draw(context) {
 		context.drawImage(this.image, this.x, this.y, this.width, this.height);
@@ -67,13 +68,12 @@ class Paper extends Emoji {
 	}
 }
 class UI {
-    constructor(game)
-    {
-        this.game = game;
-    }
-    draw(context){
-		context.fillText('Rock Paper Scissors', 50,50)
-    }
+	constructor(game) {
+		this.game = game;
+	}
+	draw(context) {
+		context.fillText("Rock Paper Scissors", 50, 50);
+	}
 }
 class Game {
 	constructor(width, height) {
@@ -82,9 +82,9 @@ class Game {
 		this.scissors = [];
 		this.rocks = [];
 		this.papers = [];
-		this.numberOfScissors = 4;
-		this.numberOfRocks = 4;
-		this.numberOfPapers = 4;
+		this.numberOfScissors = 6;
+		this.numberOfRocks = 6;
+		this.numberOfPapers = 6;
 		this.speedMult = 0.5;
 		for (let i = 0; i < this.numberOfScissors; i++) {
 			this.scissors.push(new Scissor());
@@ -99,44 +99,11 @@ class Game {
 	}
 	update() {
 		//Rock Beats Scissors
-		this.scissors.forEach((scissor) => {
-			scissor.update();
-			this.rocks.forEach((rock) => {
-				if (this.checkCollision(rock, scissor)) {
-					scissor.markedForDeletion = true;
-                    this.rocks.push(new Rock());
-                    this.rocks[this.rocks.length -1].x = scissor.x
-                    this.rocks[this.rocks.length -1].y = scissor.y
-                    // this.rocks = this.rocks.filter((rock) => !rock.markedForDeletion);
-				}
-			});
-		});
+		this.BeatsLogic(this.rocks,this.scissors,Rock);
 		//Paper Beats Rock
-		this.rocks.forEach((rock) => {
-			rock.update();
-			this.papers.forEach((paper) => {
-				if (this.checkCollision(rock, paper)) {
-					rock.markedForDeletion = true;
-                    this.papers.push(new Paper());
-                    this.papers[this.papers.length -1].x = rock.x
-                    this.papers[this.papers.length -1].y = rock.y
-                    // this.papers = this.papers.filter((paper) => !paper.markedForDeletion);
-				}
-			});
-		});
+		this.BeatsLogic(this.papers,this.rocks,Paper);
 		//Scissors Beats Paper
-		this.papers.forEach((paper) => {
-			paper.update();
-			this.scissors.forEach((scissor) => {
-				if (this.checkCollision(paper, scissor)) {
-					paper.markedForDeletion = true;
-                    this.scissors.push(new Scissor());
-                    this.scissors[this.scissors.length -1].x = paper.x
-                    this.scissors[this.scissors.length -1].y = paper.y
-                    // this.scissors = this.scissors.filter((scissor) => !scissor.markedForDeletion);
-				}
-			});
-		});
+		this.BeatsLogic(this.scissors,this.papers,Scissor);
 		
 		this.scissors = this.scissors.filter((scissor) => !scissor.markedForDeletion);
 		this.rocks = this.rocks.filter((rock) => !rock.markedForDeletion);
@@ -144,95 +111,23 @@ class Game {
 	}
 	chase() {
 		//Rock Chase Scissors
-		if (this.scissors.length > 0) {
-			this.rocks.forEach((rock) => {
-				let rockTarget = this.scissors[0];
-				let shortestScissor = this.distanceSquared(rock, rockTarget);
-				for (let i = 0; i < this.scissors.length; i++) {
-					let dScissor = this.distanceSquared(rock, this.scissors[i]);
-					if (dScissor < shortestScissor) {
-						rockTarget = this.scissors[i];
-						shortestScissor = dScissor;
-					}
-				}
-				if (rock.x - rockTarget.x > 0) {
-					rock.speedX = Math.random() * -this.speedMult;
-				} else {
-					rock.speedX = Math.random() * this.speedMult;
-				}
-				if (rock.y - rockTarget.y > 0) {
-					rock.speedY = Math.random() * -this.speedMult;
-				} else {
-					rock.speedY = Math.random() * this.speedMult;
-				}
-			});
-		}
+		this.ChaseLogic(this.rocks,this.scissors);
 		if (this.scissors.length === 0) {
 			this.rocks.forEach((rock) => {
 				rock.speedX = 0;
 				rock.speedY = 0;
 			});
 		}
-
 		//Paper Chase Rock
-		if (this.rocks.length > 0) {
-			this.papers.forEach((paper) => {
-				let paperTarget = this.rocks[0];
-				let shortestRock = this.distanceSquared(paper, paperTarget);
-				for (let i = 0; i < this.rocks.length; i++) {
-					let dRock = this.distanceSquared(paper, this.rocks[i]);
-					if (dRock < shortestRock) {
-						paperTarget = this.rocks[i];
-						shortestRock = dRock;
-					}
-				}
-				if (paper.x - paperTarget.x > 0) {
-					paper.speedX = Math.random() * -this.speedMult;
-				} else {
-					paper.speedX = Math.random() * this.speedMult;
-				}
-				if (paper.y - paperTarget.y > 0) {
-					paper.speedY = Math.random() * -this.speedMult;
-				} else {
-					paper.speedY = Math.random() * this.speedMult;
-				}
-			});
-		}
+		this.ChaseLogic(this.papers,this.rocks);
 		if (this.rocks.length === 0) {
 			this.papers.forEach((paper) => {
 				paper.speedX = 0;
 				paper.speedY = 0;
 			});
 		}
-
 		//Scissors Chase Paper
-		if (this.papers.length > 0) {
-			this.scissors.forEach((scissor) => {
-				let scissorTarget = this.papers[0];
-				let shortestPaper = this.distanceSquared(
-					scissor,
-					scissorTarget
-				);
-				for (let i = 0; i < this.papers.length; i++) {
-					let dPaper = this.distanceSquared(scissor, this.papers[i]);
-					if (dPaper < shortestPaper) {
-						scissorTarget = this.papers[i];
-						shortestPaper = dPaper;
-					}
-				}
-				if (scissor.x - scissorTarget.x > 0) {
-					scissor.speedX = Math.random() * -this.speedMult;
-				} else {
-					scissor.speedX = Math.random() * this.speedMult;
-				}
-				if (scissor.y - scissorTarget.y > 0) {
-					scissor.speedY = Math.random() * -this.speedMult;
-				} else {
-					scissor.speedY = Math.random() * this.speedMult;
-				}
-			});
-		}
-
+		this.ChaseLogic(this.scissors,this.papers);
 		if (this.papers.length === 0) {
 			this.scissors.forEach((scissor) => {
 				scissor.speedX = 0;
@@ -260,6 +155,49 @@ class Game {
 			(rect1.y - rect2.y) * (rect1.y - rect2.y)
 		);
 	}
+	BeatsLogic(attackerArray, defenderArray, attackerSpawnObject){
+		defenderArray.forEach((defender) => {
+			defender.update();
+			attackerArray.forEach((attacker) => {
+				if (this.checkCollision(attacker,defender)) {
+					defender.markedForDeletion = true;
+					attacker.targetLocked = false;
+					attackerArray.push(new attackerSpawnObject());
+					attackerArray[attackerArray.length -1].x = defender.x;
+					attackerArray[attackerArray.length -1].y = defender.y;
+				}
+
+			});
+		});
+	}
+	ChaseLogic(attackersArray, defendersArray){
+		if (defendersArray.length > 0) {
+			attackersArray.forEach((attacker) => {
+					let target = defendersArray[0];
+				//if (attacker.targetLocked === false) {
+					let initialDistance = this.distanceSquared(attacker, target);
+					for (let i = 0; i < defendersArray.length; i++) {
+						let newDistance = this.distanceSquared(attacker, defendersArray[i]);
+						if (newDistance < initialDistance) {
+							target = defendersArray[i];
+							initialDistance = newDistance;
+							//attacker.targetLocked = true;
+						}
+					//}
+					}
+					if (attacker.x - target.x > 0){
+						attacker.speedX = Math.random() * -this.speedMult;
+					} else {
+						attacker.speedX = Math.random() * this.speedMult;
+					}
+					if (attacker.y - target.y > 0){
+						attacker.speedY = Math.random() * -this.speedMult;
+					} else {
+						attacker.speedY = Math.random() * this.speedMult;
+					}		
+			});
+		}
+	}
 }
 
 const game = new Game(canvas.width, canvas.height);
@@ -268,8 +206,9 @@ const game = new Game(canvas.width, canvas.height);
 function animate() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	game.draw(ctx);
-	game.update();
 	game.chase();
+	game.update();
+	
 	requestAnimationFrame(animate);
 }
 
